1a8e62c46876a09d7cb343ac5c1d00c53869bfcd
Mike <mike.ash@lexicon.se>
2017-02-07 15:24:29 +0100

    _CodeFirst_rdy

---
+++ Semi_Webshop/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Semi_Webshop.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Semi_Webshop.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Semi_Webshop.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Semi_Webshop.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- Semi_Webshop/Models/AccountViewModels.cs
+++ Semi_Webshop/Models/AccountViewModels.cs
@@ -79,6 +79,11 @@ namespace Semi_Webshop.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name ="Full Name")]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
