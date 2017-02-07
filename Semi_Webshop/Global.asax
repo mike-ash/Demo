494dfaeb43771d9d963e5a1f32d74865c385156d
Konsult <Konsult@LEXSMAKON2>
2017-02-07 13:27:08 +0100

    Add project files.

---
+++ Semi_Webshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Semi_Webshop.MvcApplication" Language="C#" %>
---
+++ Semi_Webshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Semi_Webshop.Startup))]
+namespace Semi_Webshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
