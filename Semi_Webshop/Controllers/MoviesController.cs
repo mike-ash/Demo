3175c91ab88161b0f91678f270434c3f1cd7ffed
Mike <mike.ash@lexicon.se>
2017-02-07 13:47:51 +0100

    _Bootstrapped

---
+++ Semi_Webshop/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Semi_Webshop.Models;
+using System.Web.Mvc;
+
+namespace Semi_Webshop.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Terminator I" };
+            return View(movie);
+        }
+    }
+}
