9adc87b1dbdb3445b88e222112dae5f62340e6ac
Mike <mike.ash@lexicon.se>
2017-02-09 08:27:33 +0100

    _Deals

---
+++ Semi_Webshop/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Semi_Webshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Semi_Webshop.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Movie> Deals { get; set; }
+    }
+}
