781804ad4015031d92ae601eff58c8b99a3033d4
Mike <mike.ash@lexicon.se>
2017-05-03 09:23:40 +0200

    _New_Services

--- Semi_Webshop/Controllers/ShoppingCartController.cs
+++ Semi_Webshop/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Semi_Webshop.services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,10 @@ namespace Semi_Webshop.Controllers
 {
     public class ShoppingCartController : Controller
     {
+
+        protected readonly IShoppingCartService shoppingCartService;
+        protected readonly IMovieService catalogService;
+        protected readonly IFinanceService financeService;
         // GET: ShoppingCart
         public ActionResult Index()
         {
---
+++ Semi_Webshop/services/IFinanceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semi_Webshop.services
+{
+    public interface IFinanceService
+    {
+        double CalculateTax(double price);
+        double Tax { get; }
+    }
+}
