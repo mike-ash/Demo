5b4d5a3c375254316dc9a5f13e76d451d036f517
Mike <mike.ash@lexicon.se>
2017-05-03 09:41:25 +0200

    _Tax_Implementation

---
+++ Semi_Webshop/services/Implementation/FinanceService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Semi_Webshop.services
+{
+    public class FinanceService : IFinanceService
+    {
+        public double CalculateTax(double price)
+        {
+            return Tax * price;
+        }
+
+        protected double? tax = null;
+
+        public double Tax
+        {
+            get
+            {
+                if (!tax.HasValue)
+                {
+                    double taxAux;
+                    if (double.TryParse(WebConfigurationManager.AppSettings["mike:tax"], out taxAux))
+                        tax = taxAux;
+                    else
+                        tax = 0;
+                }
+                return tax.Value;
+            }
+        }
+
+    }
+}
